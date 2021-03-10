7ebc8b8663cac41b29b61eb4395e87631bd472a7
Levi Daymon <levithehandyman@outlook.com>
2021-03-10 15:58:22 -0500

    bunch of new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeypadComponent from './components/KeypadComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result:''
+    }
+  }
+
+  onClick = button => {
+
+    if(button === '='){
+        this.calculate()
+    }
+
+    else if(button === 'AC'){
+        this.reset()
+    }
+    else if(button === 'back'){
+        this.backspace()
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
+  calculate = () => {
+    try {
+        this.setState({
+            result: (eval(this.state.result) || '' ) + '' //NEVER use eval! unless you dont super care because its a one page calculator..
+        })
+    } catch (e) {
+        this.setState({
+            result: 'error'
+        })
+
+    }
+};
+
+reset = () => {
+    this.setState({
+        result: ''
+    })
+};
+
+backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+};
+
+  render(){
+    return (
+      <div>
+        <div className='calculator-body' id='calculator'>
+          <h1>js calculator</h1>
+          <ResultComponent result={this.state.result}/>
+          <KeypadComponent onClick={this.onClick} />
+        </div>
+      </div>
   );
+
+  }  
 }
 
 export default App;
